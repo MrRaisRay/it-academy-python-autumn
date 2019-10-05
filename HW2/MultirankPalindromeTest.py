21d66262f8e3deb1db4c7cd0470ac262a96a25a2
Valentine <raisen95@gmail.com>
2019-10-06 00:10:12 +0300

    Fix palindrome test

--- HW2/MultirankPalindromeTest.py
+++ HW2/MultirankPalindromeTest.py
@@ -4,7 +4,8 @@ dopel = 0
 while 1:
     dopel += num % 10
     num //= 10
-    if num / 10 <= 0:
+    if num / 10 < 1:
+        dopel = dopel * 10 + num % 10
         break
     dopel *= 10
 if test == dopel:
