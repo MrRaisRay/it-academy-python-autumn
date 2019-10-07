49b50e2270bd648398370eedb500ced8ef51db2d
Valentine <raisen95@gmail.com>
2019-10-07 23:05:08 +0300

    Push HW4

--- HW3/Big_n_SmallCounter.py
+++
@@ -1,8 +0,0 @@
-data = str(input('Enter text: '))
-small = big = 0
-for i in data:
-    if 'a' <= i <= 'z':
-        small += 1
-    elif 'A' <= i <= 'Z':
-        big += 1
-print(f'Small chars: {small} Big chars: {big}')
--- HW3/UnnesseyCharsRemover.py
+++
@@ -1,8 +0,0 @@
-data = str(input()).replace(' ', '')
-targetstr = ''
-trash = ''
-for i in data:
-    if trash.find(i) == -1:
-        targetstr += i
-        trash += i
-print(targetstr)
