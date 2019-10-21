1da0527714931efc36dd7c8f9601a6952566d47e
Valentine <raisen95@gmail.com>
2019-10-21 20:57:45 +0300

    Add HW5

--- HW4/ListPractice.py
+++
@@ -1,9 +0,0 @@
-lst1 = [i + j for i in ['a', 'b'] for j in ['b', 'c', 'd']]
-print(lst1)
-print(lst1[::2])
-lst2 = [str(i) + 'a' for i in range(1, 5, 1)]
-print(lst2)
-print(lst2.pop(1))
-lst3 = lst2[::]
-lst3.insert(1, 'a2')
-print(str(lst3) + '\n' + str(lst2))
---
+++ HW5/ElemenIn1Lists.py
@@ -0,0 +1,3 @@
+lst1 = set(input().split())
+lst2 = set(input().split())
+print(len(lst1 ^ lst2))
